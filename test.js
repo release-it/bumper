d27f0eaf6bd9356dbc3ea8635d0f4af830e74190
Lars Kappert <lars@webpro.nl>
2020-04-29 15:30:43 +0200

    Replace ava with bron

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const { EOL } = require('os');
-const test = require('ava');
+const assert = require('assert').strict;
+const test = require('bron');
 const mock = require('mock-fs');
 const { factory, runTasks } = require('release-it/test/util');
 const Plugin = require('.');
@@ -15,79 +16,75 @@ mock({
 
 const namespace = 'bumper';
 
-const readFile = file =>
-  fs
-    .readFileSync(file)
-    .toString()
-    .trim();
+const readFile = file => fs.readFileSync(file).toString().trim();
 
-test('should not throw', async t => {
+test('should not throw', async () => {
   const options = { [namespace]: {} };
   const plugin = factory(Plugin, { namespace, options });
-  await t.notThrowsAsync(runTasks(plugin));
+  await assert.doesNotReject(runTasks(plugin));
 });
 
-test('should return latest version from JSON file', async t => {
+test('should return latest version from JSON file', async () => {
   const options = { [namespace]: { in: './bower.json' } };
   const plugin = factory(Plugin, { namespace, options });
   const version = await plugin.getLatestVersion();
-  t.is(version, '1.0.0');
+  assert.equal(version, '1.0.0');
 });
 
-test('should return latest version from plain text file', async t => {
+test('should return latest version from plain text file', async () => {
   const options = { [namespace]: { in: { file: './foo.txt', type: 'text/plain' } } };
   const plugin = factory(Plugin, { namespace, options });
   const version = await plugin.getLatestVersion();
-  t.is(version, '2.0.0');
+  assert.equal(version, '2.0.0');
 });
 
-test('should write indented JSON file', async t => {
+test('should write indented JSON file', async () => {
   const options = { [namespace]: { out: './manifest.json' } };
   const plugin = factory(Plugin, { namespace, options });
   await plugin.bump('1.2.3');
-  t.is(readFile('./manifest.json'), `{${EOL}  "version": "1.2.3"${EOL}}`);
+  assert.equal(readFile('./manifest.json'), `{${EOL}  "version": "1.2.3"${EOL}}`);
 });
 
-test('should write new, indented JSON file', async t => {
+test('should write new, indented JSON file', async () => {
   const options = { [namespace]: { out: ['./null.json'] } };
   const plugin = factory(Plugin, { namespace, options });
   await plugin.bump('0.0.0');
-  t.is(readFile('./null.json'), `{${EOL}  "version": "0.0.0"${EOL}}`);
+  assert.equal(readFile('./null.json'), `{${EOL}  "version": "0.0.0"${EOL}}`);
 });
 
-test('should write version at path', async t => {
+test('should write version at path', async () => {
   const options = { [namespace]: { out: { file: './deep.json', path: 'deep.sub.version' } } };
   const plugin = factory(Plugin, { namespace, options });
   await plugin.bump('1.2.3');
-  t.is(readFile('./deep.json'), JSON.stringify({ deep: { sub: { version: '1.2.3' } } }, null, '  '));
+  assert.equal(readFile('./deep.json'), JSON.stringify({ deep: { sub: { version: '1.2.3' } } }, null, '  '));
 });
 
-test('should write plain version text file', async t => {
+test('should write plain version text file', async () => {
   const options = { [namespace]: { out: [{ file: './VERSION', type: 'text/plain' }] } };
   const plugin = factory(Plugin, { namespace, options });
   await plugin.bump('3.2.1');
-  t.is(readFile('./VERSION'), '3.2.1');
+  assert.equal(readFile('./VERSION'), '3.2.1');
 });
 
-test('should write plain text file', async t => {
+test('should write plain text file', async () => {
   const options = { [namespace]: { in: './bower.json', out: { file: './foo.php', type: 'text/php' } } };
   const plugin = factory(Plugin, { namespace, options });
   await runTasks(plugin);
-  t.is(readFile('./foo.php'), '/* comments\nversion: v1.0.1 */ <? echo <p>hello world</p>; ?>');
+  assert.equal(readFile('./foo.php'), '/* comments\nversion: v1.0.1 */ <? echo <p>hello world</p>; ?>');
 });
 
-test('should read/write plain text file', async t => {
+test('should read/write plain text file', async () => {
   const options = {
     [namespace]: { in: { file: './foo.txt', type: 'text/plain' }, out: { file: './foo.txt', type: 'text/plain' } }
   };
   const plugin = factory(Plugin, { namespace, options });
   await runTasks(plugin);
-  t.is(readFile('./foo.txt'), '2.0.1');
+  assert.equal(readFile('./foo.txt'), '2.0.1');
 });
 
-test('should not write in dry run', async t => {
+test('should not write in dry run', async () => {
   const options = { [namespace]: { in: './dryrun.json' } };
   const plugin = factory(Plugin, { namespace, options, global: { isDryRun: true } });
   await plugin.bump('1.0.1');
-  t.is(readFile('./dryrun.json'), `{"version":"1.0.0"}`);
+  assert.equal(readFile('./dryrun.json'), `{"version":"1.0.0"}`);
 });
