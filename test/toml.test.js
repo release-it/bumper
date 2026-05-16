9760ee9a04b893249d764b8e8481876e3dff81a7
James Barr <James.Matthew.Barr@gmail.com>
2026-05-16 23:40:49 +0100

    test: refactor toml test to reduce boilerplate

--- test/toml.test.js
+++ test/toml.test.js
@@ -20,6 +20,17 @@ mock({
   './pyproject.toml': `[project]${EOL}name = "foo"${EOL}version = "${CURRENT_VERSION}"${EOL}authors = [{ name = "Alice", email = "a@example.com" }]${EOL}dependencies = [${EOL}  "django = 1${CURRENT_VERSION}"${EOL}]${EOL}[tool.commitizen]${EOL}version = "${CURRENT_VERSION}"${EOL}`,
 });
 
+const readFilePostBumperTasks = async (namespaceOptions) => {  
+  const options = {
+    [NAMESPACE]: namespaceOptions
+  }
+
+  const plugin = await factory(Bumper, { NAMESPACE, options });
+  await runTasks(plugin);
+  
+  return readFile(namespaceOptions.out.file)
+}
+
 describe('toml file', { concurrency: true }, () => {
   it('should return latest version', async () => {
     const options = {
@@ -31,137 +42,108 @@ describe('toml file', { concurrency: true }, () => {
   });
 
   it('should add version at path when missing', async () => {
-    const options = {
-      [NAMESPACE]: {
-        out: {
-          file: './without_target_version.toml',
-          type: 'text/toml',
-          path: 'tool.test.version'
-        }
+    const namespaceOptions = {
+      out: {
+        file: './without_target_version.toml',
+        type: 'text/toml',
+        path: 'tool.test.version'
       }
     };
-    const plugin = await factory(Bumper, { NAMESPACE, options });
-    await runTasks(plugin);
-    assert.equal(readFile('./without_target_version.toml'), `[tool.test]${EOL}version = "${NEW_VERSION}"${EOL}[tool.ignored]${EOL}version = "${CURRENT_VERSION}"${EOL}`);
+    const contents = await readFilePostBumperTasks(namespaceOptions)
+    assert.equal(contents, `[tool.test]${EOL}version = "${NEW_VERSION}"${EOL}[tool.ignored]${EOL}version = "${CURRENT_VERSION}"${EOL}`);
   });
 
   it('should update version at path', async () => {
-    const options = {
-      [NAMESPACE]: {
-        out: {
-          file: './foo.toml',
-          type: 'text/toml',
-          path: 'tool.test.version'
-        }
+    const namespaceOptions = {
+      out: {
+        file: './foo.toml',
+        type: 'text/toml',
+        path: 'tool.test.version'
       }
     };
-    const plugin = await factory(Bumper, { NAMESPACE, options });
-    await runTasks(plugin);
-    assert.equal(readFile('./foo.toml'), `[tool.test]${EOL}version = "${NEW_VERSION}"${EOL}`);
+    const contents = await readFilePostBumperTasks(namespaceOptions);
+    assert.equal(contents, `[tool.test]${EOL}version = "${NEW_VERSION}"${EOL}`);
   });
 
   it('should update versions at multiple paths', async () => {
-    const options = {
-      [NAMESPACE]: {
-        out: {
-          file: './with_multiple_version.toml',
-          type: 'text/toml',
-          path: ['project.version', 'tool.test.version']
-        }
+    const namespaceOptions = {
+      out: {
+        file: './with_multiple_version.toml',
+        type: 'text/toml',
+        path: ['project.version', 'tool.test.version']
       }
     };
-    const plugin = await factory(Bumper, { NAMESPACE, options });
-    await runTasks(plugin);
-    assert.equal(readFile('./with_multiple_version.toml'), `[tool.test]${EOL}version = "${NEW_VERSION}"${EOL}[project]${EOL}version = "${NEW_VERSION}"${EOL}`);
+    const contents = await readFilePostBumperTasks(namespaceOptions);
+    assert.equal(contents, `[tool.test]${EOL}version = "${NEW_VERSION}"${EOL}[project]${EOL}version = "${NEW_VERSION}"${EOL}`);
   });
 
   it('should write without defining the type', async () => {
-    const options = {
-      [NAMESPACE]: { out: { file: './foo.toml', path: 'tool.test.version' } }
+    const namespaceOptions = {
+      out: { file: './foo.toml', path: 'tool.test.version' }
     };
-    const plugin = await factory(Bumper, { NAMESPACE, options });
-    await runTasks(plugin);
-    assert.equal(readFile('./foo.toml'), `[tool.test]${EOL}version = "${NEW_VERSION}"${EOL}`);
+    const contents = await readFilePostBumperTasks(namespaceOptions);
+    assert.equal(contents, `[tool.test]${EOL}version = "${NEW_VERSION}"${EOL}`);
   });
 
   it('should read/write', async () => {
-    const options = {
-      [NAMESPACE]: {
-        in: { file: './foo.toml', type: 'application/toml', path: 'tool.test.version' },
-        out: { file: './foo.toml', type: 'application/toml', path: 'tool.test.version' }
-      }
+    const namespaceOptions = {
+      in: { file: './foo.toml', type: 'application/toml', path: 'tool.test.version' },
+      out: { file: './foo.toml', type: 'application/toml', path: 'tool.test.version' }
     };
-    const plugin = await factory(Bumper, { NAMESPACE, options });
-    await runTasks(plugin);
-    assert.equal(readFile('./foo.toml'), `[tool.test]${EOL}version = "${NEW_VERSION}"${EOL}`);
+    const contents = await readFilePostBumperTasks(namespaceOptions);
+    assert.equal(contents, `[tool.test]${EOL}version = "${NEW_VERSION}"${EOL}`);
   });
 
   it('should read/write without defining the type', async () => {
-    const options = {
-      [NAMESPACE]: {
-        in: { file: './foo.toml', path: 'tool.test.version' },
-        out: { file: './foo.toml', path: 'tool.test.version' }
-      }
+    const namespaceOptions = {
+      in: { file: './foo.toml', path: 'tool.test.version' },
+      out: { file: './foo.toml', path: 'tool.test.version' }
     };
-    const plugin = await factory(Bumper, { NAMESPACE, options });
-    await runTasks(plugin);
-    assert.equal(readFile('./foo.toml'), `[tool.test]${EOL}version = "${NEW_VERSION}"${EOL}`);
+    const contents = await readFilePostBumperTasks(namespaceOptions);
+    assert.equal(contents, `[tool.test]${EOL}version = "${NEW_VERSION}"${EOL}`);
   });
 
   it('should read/write without formatting', async () => {
-    const options = {
-      [NAMESPACE]: {
-        in: { file: './with_comments_and_formatting.toml', path: 'tool.test.version' },
-        out: { file: './with_comments_and_formatting.toml', path: 'tool.test.version' }
-      }
+    const namespaceOptions = {
+      in: { file: './with_comments_and_formatting.toml', path: 'tool.test.version' },
+      out: { file: './with_comments_and_formatting.toml', path: 'tool.test.version' }
     };
-    const plugin = await factory(Bumper, { NAMESPACE, options });
-    await runTasks(plugin);
+    const contents = await readFilePostBumperTasks(namespaceOptions);
     assert.equal(
-      readFile('./with_comments_and_formatting.toml'),
+      contents,
       `# Lead with some comments${EOL}${EOL}[workspace]${EOL}${EOL}${EOL}[tool.test]${EOL}name    = "hello_world"${EOL}version = "${NEW_VERSION}"${EOL}`
     );
   });
 
   it('should noop when the target version is already at the new version', async () => {
-    const options = {
-      [NAMESPACE]: {
-        out: { file: './at_current_version.toml', path: 'tool.test.version' }
-      }
+    const namespaceOptions = {
+      out: { file: './at_current_version.toml', path: 'tool.test.version' }
     };
-    const plugin = await factory(Bumper, { NAMESPACE, options });
-    await runTasks(plugin);
-
+    const contents = await readFilePostBumperTasks(namespaceOptions);
     assert.equal(
-      readFile('./at_current_version.toml'),
+      contents,
       `[tool.test]${EOL}version = "${NEW_VERSION}"${EOL}dependencies = [${EOL}  "django = 1${CURRENT_VERSION}"${EOL}]${EOL}`
     );
-  })
+  });
 
   it('should handle example cargo.toml file', async () => {
-    const options = {
-      [NAMESPACE]: {
-        out: { file: './cargo.toml', path: 'package.version' }
-      }
+    const namespaceOptions = {
+      out: { file: './cargo.toml', path: 'package.version' }
     };
-    const plugin = await factory(Bumper, { NAMESPACE, options });
-    await runTasks(plugin);
+    const contents = await readFilePostBumperTasks(namespaceOptions);
     assert.equal(
-      readFile('./cargo.toml'),
+      contents,
       `[workspace]${EOL}${EOL}[package]${EOL}name = "hello_world"${EOL}version = "${NEW_VERSION}"${EOL}authors = [ "Alice <a@example.com>", "Bob <b@example.com>" ]${EOL}${EOL}[dependencies]${EOL}time = "0.1.12"${EOL}`
     );
   });
 
   it('should handle example pyproject.toml file', async () => {
-    const options = {
-      [NAMESPACE]: {
-        out: { file: './pyproject.toml', path: 'project.version' }
-      }
+    const namespaceOptions = {
+      out: { file: './pyproject.toml', path: 'project.version' }
     };
-    const plugin = await factory(Bumper, { NAMESPACE, options });
-    await runTasks(plugin);
+    const contents = await readFilePostBumperTasks(namespaceOptions);
     assert.equal(
-      readFile('./pyproject.toml'),
+      contents,
       `[project]${EOL}name = "foo"${EOL}version = "${NEW_VERSION}"${EOL}authors = [{ name = "Alice", email = "a@example.com" }]${EOL}dependencies = [${EOL}  "django = 1${CURRENT_VERSION}"${EOL}]${EOL}[tool.commitizen]${EOL}version = "${CURRENT_VERSION}"${EOL}`
     );
   });
